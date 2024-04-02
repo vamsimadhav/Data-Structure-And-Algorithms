1082dd73b332e4ce11449871e85b0cb2c1670d80
vamsimadhav <vamsimadhav2000@gmail.com>
2024-04-02 22:32:36 +0530

    startred with patterns

---
+++ Striver/A-Z/Step 1/Patterns/pattern_1.py
@@ -0,0 +1,10 @@
+def nForest(n:int) ->None:
+    # Write your solution here.
+    i = 0
+    for i in range(n):
+        j = 0
+        for j in range(n):
+            print("* ",end='')
+            j+=1
+        print()
+        i+=1
